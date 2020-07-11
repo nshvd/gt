98d292310c535355f60440d4b2811cc68f5e6c70
smrtmrt <smart.marat@gmail.com>
2020-07-10 22:53:50 -0400

    reproduce conflict 1

--- src/p1/C1.java
+++ src/p1/C1.java
@@ -3,7 +3,7 @@ package p1;
 public class C1 {
     public static void main(String[] args) {
         System.out.println("Hello new world!");
-        System.out.println("Bye");
+        System.out.println("Bye new world");
         System.out.println("Conflict here");
         hl("Nazar");
         System.out.println("abc");
