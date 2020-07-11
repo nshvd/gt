6a28f5b028c8bc21cabab710562e367cee173421
smrtmrt <smart.marat@gmail.com>
2020-07-11 15:55:18 -0400

    test 1

--- src/p1/C3.java
+++ src/p1/C3.java
@@ -6,6 +6,6 @@ public class C3 {
     public static void sayHello(String name){
         System.out.println("Hello: " + name);
         System.out.println("abc");
-        System.out.println("Trying to resolve conflict");
+        System.out.println("GIVE ME A CONFLICT!");
     }
 }
