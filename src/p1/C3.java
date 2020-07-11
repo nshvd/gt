6c9429305452e7fab2c98e87ea766b322d8dd39f
Nursultan Altynbekov <naltynbekof@gmail.com>
2020-07-11 14:41:50 -0500

    try to resolve conf

--- src/p1/C3.java
+++ src/p1/C3.java
@@ -5,5 +5,6 @@ import sun.misc.Unsafe;
 public class C3 {
     public static void sayHello(String name){
         System.out.println("Hello: " + name);
+        System.out.println("Trying to resolve conflict");
     }
 }
