67ac1e8e2aed9f0b5d95b9c2c2b3edd8019a1763
smrtmrt <smart.marat@gmail.com>
2020-07-10 20:53:05 -0400

    conflict

--- src/p1/C1.java
+++ src/p1/C1.java
@@ -4,6 +4,7 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hello new world!");
         System.out.println("Bye");
+        hl("Nazar");
     }
 
     public static void hl(String name){
