22ca1a10314bd0f6f31d6d5075a696434826c920
Yuliana <juliana100396@gnail.com>
2020-07-14 16:37:55 -0500

    conflict2

--- src/p1/C5.java
+++ src/p1/C5.java
@@ -3,6 +3,6 @@ package p1;
 public class C5 {
     public static void main(String[] args) {
         System.out.println("New class");
-        System.out.println("Conflict");
+        System.out.println("Conflict20");
     }
 }
